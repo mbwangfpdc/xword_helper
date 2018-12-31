ca64223a9fff69ff1e5d41c5b2454aaac3e644a6
Morgan Borjigin-Wang <mbwang10@gmail.com>
2018-12-31 18:24:27 -0500

    just syncing up

---
+++ test.py
@@ -0,0 +1,18 @@
+from nltk.corpus import wordnet as wn
+import math
+
+ALPHA = .2
+BETA = .45
+
+
+def similarity(word1, word2):
+    math.exp(-ALPHA * ~~~length)
+    math.e
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
