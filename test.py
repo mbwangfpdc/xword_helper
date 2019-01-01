3e709f95d56b8f2093a984f5adaa4df5eef12405
Kavin Pawittranon <kavinp@umich.edu>
2018-12-31 19:01:59 -0500

    12/31/18 7:01

--- test.py
+++ test.py
@@ -5,14 +5,27 @@ ALPHA = .2
 BETA = .45
 
 
-def similarity(word1, word2):
-    math.exp(-ALPHA * ~~~length)
-    math.e
+# takes two synsets
+def similarity(syn1, syn2):
+    l_factor = math.exp(-ALPHA * syn1.shortest_path_distance(syn2))
+
+    lch_height = syn1.lowest_common_hypernyms(syn2).min_depth()
+    h_power = BETA * lch_height
+    pos_h_exp = math.exp(h_power)
+    neg_h_exp = 1 / pos_h_exp
+    h_factor = (pos_h_exp - neg_h_exp) / (pos_h_exp + neg_h_exp)
+
+    return l_factor * h_factor
+
+class nlpSolver:
+    """Hold synsets for clues and candidates"""
+    def __init__(clue, crossWord): # takes crossword clue as sentence
+        self.clue_synsets = [wn.synsets(word)[0] for word in clue.split(" ")]
+        self.candidate_synset = None    # possible solutions to check
+
+
 
 
 def main():
-    pass
 
-
-if __name__ == "__main__":
-    main()
+main()
